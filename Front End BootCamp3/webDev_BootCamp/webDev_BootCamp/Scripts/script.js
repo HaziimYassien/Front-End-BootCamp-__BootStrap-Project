6c402532bf49679c01554cccdd07f1f79632d161
HaziimYassien <haziim.yassien@gmail.com>
2024-01-14 00:38:44 +0200

    DONE AND DONE

---
+++ Front End BootCamp3/webDev_BootCamp/webDev_BootCamp/Scripts/script.js
@@ -0,0 +1,9 @@
+﻿const sbmtBtn = document.querySelector("#sbmtBtn")
+const firstNameInput = document.querySelector("#first-name-input")
+const firstNameOutput = document.querySelector("#first-name-output")
+
+sbmtBtn.addEventListener("click", function () {
+    alert(firstNameInput);
+})  
+
+//use local storage
